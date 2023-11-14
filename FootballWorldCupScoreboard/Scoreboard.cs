40a9bf445191acc4288494affa697f92afcafdee
Nikolay Sinuk <silverwolfkj2015@gmail.com>
2023-11-14 05:45:44 +0100

    impl handler for null&empty team name

--- FootballWorldCupScoreboard/Scoreboard.cs
+++ FootballWorldCupScoreboard/Scoreboard.cs
@@ -4,6 +4,12 @@
     {
         public object Start(string homeTeamName, string awayTeamName)
         {
+            if (string.IsNullOrEmpty(homeTeamName))
+                throw new ArgumentException("The team name can't be empty or null", nameof(homeTeamName));
+
+            if (string.IsNullOrEmpty(awayTeamName))
+                throw new ArgumentException("The team name can't be empty or null", nameof(awayTeamName));
+
             return new { homeTeamName, awayTeamName };
         }
     }
