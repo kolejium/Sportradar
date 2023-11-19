7fdaf0b0f4fdbc31efbe99203b6e1e6c3a1d358a
Nikolay Sinuk <silverwolfkj2015@gmail.com>
2023-11-19 08:37:04 +0100

    Created new test to provide conds to check func of updating match score.

---
+++ FootballWorldCupScoreboard.Tests/MatchTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace FootballWorldCupScoreboard.Tests;
+
+public class MatchTests
+{
+    [Theory, MemberData(nameof(Scores))]
+    public void UpdateScore_OnSuccess_ShouldChangeValuesOfProperties(int newHomeTeamScore, int newAwayTeamScore)
+    {
+        var match = new Match("NYFC", "Arcenal");
+
+        match.UpdateScore(3, 5);
+        
+        Assert.True(match.HomeTeamScore == 3);
+        Assert.True(match.AwayTeamScore == 5);
+    }
+
+
+    public static IEnumerable<object[]> Scores =>
+        new List<object[]>
+        {
+            new object[] { 1, 1 },
+            new object[] { 3, 8 },
+            new object[] { 0, 9 }
+        };
+}
