00fe1ce8fbc1fd161f4ade6a252acaa5e0a61221
Nikolay Sinuk <silverwolfkj2015@gmail.com>
2023-11-20 09:16:10 +0100

    ref + my resharper styling

--- FootballWorldCupScoreboard.Tests/MatchTests.cs
+++ FootballWorldCupScoreboard.Tests/MatchTests.cs
@@ -1,32 +1,16 @@
-﻿using FluentAssertions;
-
-namespace FootballWorldCupScoreboard.Tests;
+﻿namespace FootballWorldCupScoreboard.Tests;
 
 public class MatchTests
 {
-    [Theory, MemberData(nameof(Scores))]
-    public void UpdateScore_OnSuccess_ShouldChangeValuesOfProperties(int newHomeTeamScore, int newAwayTeamScore)
-    {
-        // Arrange
-        var match = new Match("NYFC", "Arcenal");
+    #region [ Properties ]
 
-        // Act
-        match.UpdateScore((byte) newHomeTeamScore, (byte) newAwayTeamScore);
-        
-        // Assert
-        Assert.True(match.HomeTeamScore == newHomeTeamScore);
-        Assert.True(match.AwayTeamScore == newAwayTeamScore);
-    }
-
-    [Theory, MemberData(nameof(InvalidScores))]
-    public void UpdateScore_Throw_ArgumentOutOfRangeException(int newHomeTeamScore, int newAwayTeamScore)
-    {
-        // Arrange
-        var match = new Match("NYFC", "Arcenal");
-
-        // Act && Assert
-        Assert.Throws<ArgumentOutOfRangeException>(() => match.UpdateScore(newHomeTeamScore, newAwayTeamScore));
-    }
+    public static IEnumerable<object[]> InvalidScores =>
+        new List<object[]>
+        {
+            new object[] { -10, 0 },
+            new object[] { 0, 2000 },
+            new object[] { 2000, -10 }
+        };
 
 
     public static IEnumerable<object[]> Scores =>
@@ -37,11 +21,31 @@ public class MatchTests
             new object[] { 0, 9 }
         };
 
-    public static IEnumerable<object[]> InvalidScores =>
-        new List<object[]>
-        {
-            new object[] { -10, 0 },
-            new object[] { 0, 2000 },
-            new object[] { 2000, -10 },
-        };
+    #endregion
+
+    [Theory]
+    [MemberData(nameof(Scores))]
+    public void UpdateScore_OnSuccess_ShouldChangeValuesOfProperties(int newHomeTeamScore, int newAwayTeamScore)
+    {
+        // Arrange
+        var match = new Match("NYFC", "Arcenal");
+
+        // Act
+        match.UpdateScore((byte)newHomeTeamScore, (byte)newAwayTeamScore);
+
+        // Assert
+        Assert.True(match.HomeTeamScore == newHomeTeamScore);
+        Assert.True(match.AwayTeamScore == newAwayTeamScore);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidScores))]
+    public void UpdateScore_Throw_ArgumentOutOfRangeException(int newHomeTeamScore, int newAwayTeamScore)
+    {
+        // Arrange
+        var match = new Match("NYFC", "Arcenal");
+
+        // Act && Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => match.UpdateScore(newHomeTeamScore, newAwayTeamScore));
+    }
 }
--- FootballWorldCupScoreboard.Tests/ScoreboardTests.cs
+++ FootballWorldCupScoreboard.Tests/ScoreboardTests.cs
@@ -1,221 +1,206 @@
 using FluentAssertions;
 
-namespace FootballWorldCupScoreboard.Tests
+namespace FootballWorldCupScoreboard.Tests;
+
+public class ScoreboardTests
 {
-    public class ScoreboardTests
+    public static IEnumerable<object[]> GetData(int count)
     {
-        [Fact]
-        public void Start_OnSuccess_ReturnsNotNull()
+        var dataset = new[]
         {
-            // Arrange
+            ("Dinamo", "Arsenal"),
+            ("Barcelona", "Madrid"),
+            ("NYT", "CaliforniaBikers"),
+            ("Juventus", "Real Madrid"),
+            ("AJAX", "Portu")
+        };
 
-            var scoreboard = new Scoreboard();
+        yield return new object[] { dataset };
+    }
 
-            // Act
+    public static IEnumerable<object[]> GetData2(int count)
+    {
+        var dataset = new[]
+        {
+            ("Mexico", "Canada", 0, 5),
+            ("Spain", "Brazil", 10, 2),
+            ("Germany", "France", 2, 2),
+            ("Uruguay", "Italy", 6, 6),
+            ("Argentine", "Australia", 3, 1)
+        };
 
-            var result = scoreboard.Start("Barcelona", "Madrid");
+        yield return new object[] { dataset };
+    }
 
-            // Assert
+    [Fact]
+    public void Finish_RemovesMatchFromMatchesProperty()
+    {
+        // Arrange
+        var scoreboard = new Scoreboard();
+        var match = scoreboard.Start("TeamA", "TeamB");
 
-            result.Should().NotBeNull();
+        // Act
+        var matchFinished = scoreboard.Finish(match);
+
+        // Assert
+        Assert.True(matchFinished);
+        Assert.DoesNotContain(match, scoreboard.Matches);
+    }
+
+    [Fact]
+    public void Finish_Throw_WhenArgumentIsNull()
+    {
+        // Arrange
+        var scoreboard = new Scoreboard();
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => scoreboard.Finish(null));
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData2), 5)]
+    public void Get_ReturnsMatchesOrderedByTheirScore(IEnumerable<(string, string, int, int)> enumerable)
+    {
+        // Arrange
+        var scoreboard = new Scoreboard();
+        var i = 0;
+        var count = enumerable.Count();
+
+        foreach (var item in enumerable)
+        {
+            scoreboard.Start(item.Item1, item.Item2).UpdateScore((byte)(count - i), (byte)(count - i));
+            i++;
         }
 
-        [Fact]
-        public void Start_Throw_WhenTeamNamesIsNullOrEmpty()
+        // Act
+        var sorted = scoreboard.Get().ToList();
+
+        // Assert
+        for (var j = 0; j < sorted.Count - 1; j++)
+            Assert.True(sorted[j].TotalScore >= sorted[j + 1].TotalScore,
+                "Matches are not ordered by their score in descending order.");
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData2), 5)]
+    public void Get_ReturnsMatchesOrderedByTheirScore_ThanByTheirStartTime(
+        IEnumerable<(string, string, int, int)> enumerable)
+    {
+        // Arrange
+        var scoreboard = new Scoreboard();
+        var list = enumerable.ToList();
+
+        for (var i = 0; i < list.Count; i++)
+            scoreboard.Start(list[i].Item1, list[i].Item2).UpdateScore((byte)list[i].Item3, (byte)list[i].Item4);
+
+        // Act
+        var sorted = scoreboard.Get().ToList();
+
+        // Assert
+        Assert.Equal(list.Count, sorted.Count);
+
+        for (var i = 0; i < sorted.Count - 1; i++)
         {
-            // Arrange
+            Assert.True(sorted[i].TotalScore >= sorted[i + 1].TotalScore);
 
-            var scoreboard = new Scoreboard();
-
-            // Act & Assert
-
-            Assert.Throws<ArgumentException>(() => scoreboard.Start(string.Empty, null));
-            Assert.Throws<ArgumentException>(() => scoreboard.Start("Barcelona", null));
-            Assert.Throws<ArgumentException>(() => scoreboard.Start(string.Empty, "Manchester"));
-        }
-
-        [Fact]
-        public void Start_OnSuccess_ReturnsNewMatchWithInitScores()
-        {
-            // Arrange
-
-            var scoreboard = new Scoreboard();
-            var match = new Match("Barcelona", "Madrid");
-
-            // Act
-
-            var result = scoreboard.Start("Barcelona", "Madrid");
-
-            // Assert
-            
-            Assert.Equal(match.HomeTeam, result.HomeTeam);
-            Assert.Equal(match.AwayTeam, result.AwayTeam);
-            Assert.Equal(0, result.HomeTeamScore);
-            Assert.Equal(0, result.AwayTeamScore);
-        }
-
-        [Theory]
-        [MemberData(nameof(GetData), parameters: 5)]
-        public void Matches_PropertyShouldReturnAllStartedMatches(IEnumerable<(string, string)> enumerable)
-        {
-            // Arrange
-
-            var scoreboard = new Scoreboard();
-
-            // Act
-
-            foreach (var item in enumerable)
-                scoreboard.Start(item.Item1, item.Item2);
-
-            // Assert
-
-            Assert.Equal(enumerable.Count(), scoreboard.Matches.Count);
-
-            foreach (var valueTuple in enumerable)
-            {
-                var matchExists = scoreboard.Matches.Any(match =>
-                    match.HomeTeam == valueTuple.Item1 && match.AwayTeam == valueTuple.Item2);
-
-                Assert.True(matchExists, $"Match not found for teams: {valueTuple.Item1} vs {valueTuple.Item2}");
-            }
-        }
-
-        [Fact]
-        public void Finish_RemovesMatchFromMatchesProperty()
-        {
-            // Arrange
-            var scoreboard = new Scoreboard();
-            var match = scoreboard.Start("TeamA", "TeamB");
-
-            // Act
-            var matchFinished = scoreboard.Finish(match);
-
-            // Assert
-            Assert.True(matchFinished);
-            Assert.DoesNotContain(match, scoreboard.Matches);
-        }
-
-        [Fact]
-        public void Finish_Throw_WhenArgumentIsNull()
-        {
-            // Arrange
-            var scoreboard = new Scoreboard();
-
-            // Act & Assert
-            Assert.Throws<ArgumentNullException>(() => scoreboard.Finish(null));
-        }
-
-        [Theory]
-        [MemberData(nameof(GetData2), parameters: 5)]
-        public void Get_ReturnsMatchesOrderedByTheirScore(IEnumerable<(string, string, int, int)> enumerable)
-        {
-            // Arrange
-            var scoreboard = new Scoreboard();
-            Match match;
-            var i = 0;
-            var count = enumerable.Count();
-
-            foreach (var item in enumerable)
-            {
-                match = scoreboard.Start(item.Item1, item.Item2);
-
-                match.UpdateScore((byte)(count - i), (byte)(count - i));
-
-                i++;
-            }
-
-            // Act
-            var sorted = scoreboard.Get().ToList();
-
-            // Assert
-            for (var j = 0; j < sorted.Count - 1; j++)
-            {
-                Assert.True(sorted[j].TotalScore >= sorted[j + 1].TotalScore,
-                    "Matches are not ordered by their score in descending order.");
-            }
-        }
-
-        [Theory]
-        [MemberData(nameof(GetData2), parameters: 5)]
-        public void Get_ReturnsMatchesOrderedByTheirStartTimeDesc(IEnumerable<(string, string, int, int)> enumerable)
-        {
-            // Arrange
-            var scoreboard = new Scoreboard();
-
-            foreach (var item in enumerable) 
-                scoreboard.Start(item.Item1, item.Item2);
-
-            // Act
-            var sorted = scoreboard.Get().ToList();
-
-            // Assert
-            for (var i = 0; i < sorted.Count - 1; i++)
-            {
+            if (sorted[i].TotalScore == sorted[i + 1].TotalScore)
                 Assert.True(Array.IndexOf(scoreboard.Matches.ToArray(), sorted[i])
                             < Array.IndexOf(scoreboard.Matches.ToArray(), sorted[i + 1]));
-            }
         }
+    }
 
-        [Theory]
-        [MemberData(nameof(GetData2), parameters: 5)]
-        public void Get_ReturnsMatchesOrderedByTheirScore_ThanByTheirStartTime(IEnumerable<(string, string, int, int)> enumerable)
+    [Theory]
+    [MemberData(nameof(GetData2), 5)]
+    public void Get_ReturnsMatchesOrderedByTheirStartTimeDesc(IEnumerable<(string, string, int, int)> enumerable)
+    {
+        // Arrange
+        var scoreboard = new Scoreboard();
+
+        foreach (var item in enumerable)
+            scoreboard.Start(item.Item1, item.Item2);
+
+        // Act
+        var sorted = scoreboard.Get().ToList();
+
+        // Assert
+        for (var i = 0; i < sorted.Count - 1; i++)
+            Assert.True(Array.IndexOf(scoreboard.Matches.ToArray(), sorted[i])
+                        < Array.IndexOf(scoreboard.Matches.ToArray(), sorted[i + 1]));
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData), 5)]
+    public void Matches_PropertyShouldReturnAllStartedMatches(IEnumerable<(string, string)> enumerable)
+    {
+        // Arrange
+
+        var scoreboard = new Scoreboard();
+
+        // Act
+
+        foreach (var item in enumerable)
+            scoreboard.Start(item.Item1, item.Item2);
+
+        // Assert
+
+        Assert.Equal(enumerable.Count(), scoreboard.Matches.Count);
+
+        foreach (var valueTuple in enumerable)
         {
-            // Arrange
-            var scoreboard = new Scoreboard();
-            var list = enumerable.ToList();
-            Match match;
+            var matchExists = scoreboard.Matches.Any(match =>
+                match.HomeTeam == valueTuple.Item1 && match.AwayTeam == valueTuple.Item2);
 
-            for (var i = 0; i < list.Count; i++)
-            {
-                match = scoreboard.Start(list[i].Item1, list[i].Item2);
-
-                match.UpdateScore((byte)list[i].Item3, (byte)list[i].Item4);
-            }
-
-            // Act
-            var sorted = scoreboard.Get().ToList();
-
-            // Assert
-            Assert.Equal(list.Count, sorted.Count);
-
-            for (var i = 0; i < sorted.Count - 1; i++)
-            {
-                Assert.True(sorted[i].TotalScore >= sorted[i + 1].TotalScore);
-
-                if (sorted[i].TotalScore == sorted[i + 1].TotalScore)
-                {
-                    Assert.True(Array.IndexOf(scoreboard.Matches.ToArray(), sorted[i])
-                                < Array.IndexOf(scoreboard.Matches.ToArray(), sorted[i + 1]));
-                }
-            }
+            Assert.True(matchExists, $"Match not found for teams: {valueTuple.Item1} vs {valueTuple.Item2}");
         }
+    }
 
-        public static IEnumerable<object[]> GetData(int count)
-        {
-            var dataset = new[]
-            {
-                ("Dinamo", "Arsenal"),
-                ("Barcelona", "Madrid"),
-                ("NYT", "CaliforniaBikers"),
-                ("Juventus", "Real Madrid"),
-                ("AJAX", "Portu"),
-            };
+    [Fact]
+    public void Start_OnSuccess_ReturnsNewMatchWithInitScores()
+    {
+        // Arrange
 
-            yield return new object[] { dataset };
-        }
+        var scoreboard = new Scoreboard();
+        var match = new Match("Barcelona", "Madrid");
 
-        public static IEnumerable<object[]> GetData2(int count)
-        {
-            var dataset = new[]
-            {
-                ("Mexico", "Canada", 0, 5),
-                ("Spain", "Brazil", 10, 2),
-                ("Germany", "France", 2, 2),
-                ("Uruguay", "Italy", 6, 6),
-                ("Argentine", "Australia", 3, 1),
-            };
+        // Act
 
-            yield return new object[] { dataset };
-        }
+        var result = scoreboard.Start("Barcelona", "Madrid");
+
+        // Assert
+
+        Assert.Equal(match.HomeTeam, result.HomeTeam);
+        Assert.Equal(match.AwayTeam, result.AwayTeam);
+        Assert.Equal(0, result.HomeTeamScore);
+        Assert.Equal(0, result.AwayTeamScore);
+    }
+
+    [Fact]
+    public void Start_OnSuccess_ReturnsNotNull()
+    {
+        // Arrange
+
+        var scoreboard = new Scoreboard();
+
+        // Act
+
+        var result = scoreboard.Start("Barcelona", "Madrid");
+
+        // Assert
+
+        result.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Start_Throw_WhenTeamNamesIsNullOrEmpty()
+    {
+        // Arrange
+
+        var scoreboard = new Scoreboard();
+
+        // Act & Assert
+
+        Assert.Throws<ArgumentException>(() => scoreboard.Start(string.Empty, null));
+        Assert.Throws<ArgumentException>(() => scoreboard.Start("Barcelona", null));
+        Assert.Throws<ArgumentException>(() => scoreboard.Start(string.Empty, "Manchester"));
     }
 }
